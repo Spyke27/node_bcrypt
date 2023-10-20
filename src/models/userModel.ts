d3032e6265bd8dc6bb5697e917004aa8864b52fb
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-10-20 13:57:29 -0300

    feat: add userModel

---
+++ src/models/userModel.ts
@@ -0,0 +1,39 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../db/pg";
+
+export interface UserInstance extends Model {
+    id: number;
+    nome: string;
+    email: string;
+    telefone: string;
+    idade: number;
+    cadastro: Date | string
+}
+
+export const User = sequelize.define<UserInstance>("User", {
+    id: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: DataTypes.INTEGER
+    },
+    nome: {
+        type: DataTypes.STRING
+    },
+    email: {
+        type: DataTypes.STRING
+    },
+    telefone: {
+        type: DataTypes.STRING
+    },
+    idade: {
+        type: DataTypes.INTEGER
+    },
+    cadastro: {
+        type: DataTypes.DATE
+    }
+},
+{
+    tableName: "usuarios",
+    freezeTableName: false, 
+    timestamps: false
+});
