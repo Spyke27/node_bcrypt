88ae9b63878a426134cdbb74803a5bc003944ed0
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-10-23 22:32:33 -0300

    feat: connection db postgre

--- src/db/pg.ts
+++ src/db/pg.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 //Config variaveis de ambiente
 dotenv.config();
 
-export const sequelize = new Sequelize(
+export const sequelize = new Sequelize( 
     process.env.PG_DB as string,
     process.env.PG_USER as string,
     process.env.PG_PASSWORD as string,
