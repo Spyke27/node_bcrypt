03caac1a847d8432cfdd7b077f9957bdb4377c83
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-10-23 22:33:36 -0300

    feat: add route login

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,15 @@
-import express from 'express';
-import { getAll } from '../controllers/users';
+import express, { Response, Request} from 'express';
+import { createUser, deleteUser, getAll, getUserById, getUserByName, updateUser } from '../controllers/userController';
+import { login } from '../auth/login';
 
 export const routes = express();
 
-routes.get('/', getAll);
+routes.get('/usuarios', getAll)
+routes.get('/usuarios/id/:id', getUserById)
+routes.get('/usuarios/nome/:nome', getUserByName)
+
+routes.post('/usuarios/cadastrar', createUser)
+routes.put('/usuarios/atualizar/:id', updateUser)
+routes.delete('/usuarios/deletar/:id', deleteUser)
+
+routes.post('/usuarios/login', login)
