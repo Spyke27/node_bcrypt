48315306da046241c7eaf2ad1d91aea95e60d20c
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-10-24 21:34:13 -0300

    feat: add criptografarSenha

--- src/auth/login.ts
+++ src/auth/login.ts
@@ -1,6 +1,8 @@
 import { verificarSenha } from './bcrypt'
 import { Request, Response } from 'express'
 import { User } from '../models/userModel'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
 
 export const login = async (req: Request, res: Response) => {
     const { email, senha } = req.body
@@ -12,7 +14,19 @@ export const login = async (req: Request, res: Response) => {
             const autorizado = await verificarSenha(senha, user.senha)
 
             if(autorizado){
-                return res.status(200).json(`Bem vindo, ${user.nome}`)
+                const token = jwt.sign({id: user.id}, process.env.PASSWORD_TOKEN as string, {expiresIn: '24h'})
+
+                const usuario = {
+                    id: user.id,
+                    nome: user.nome,
+                    email: user.email,
+                    telefone: user.telefone,
+                    cadastro: user.cadastro,
+                    empresa_id: user.empresa_id,
+                    data_nasc: user.data_nasc
+                }
+
+                return res.status(200).json(usuario);
             } 
             else {
                 return res.status(401).json(`Senha incorreta!`)
