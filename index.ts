3400e8b9bdb71db65a7430918d97f5d68743cdde
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-10-24 21:33:39 -0300

    start application

--- index.ts
+++ index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import path from 'path';
-import { routes } from './src/routes/user';
+import { userRoute } from './src/routes/user';
+import { empresaRoute } from './src/routes/empresa'; 
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json())
-app.use(routes)
+app.use(userRoute)
+app.use(empresaRoute)
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT || 3000)
