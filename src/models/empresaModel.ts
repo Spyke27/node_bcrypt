1ab8403333a42f6eda178223700b90ed73a46045
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-10-24 21:37:11 -0300

    feat: add empresaModel

---
+++ src/models/empresaModel.ts
@@ -0,0 +1,45 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../db/pg";
+import { User } from "./userModel";
+
+export interface EmpresaInstance extends Model {
+    id: number;
+    cnpj: string;
+    nome: string;
+    email: string;
+    senha: string;
+    cadastro: Date | string;
+    sobre: string
+}
+
+export const Empresa = sequelize.define<EmpresaInstance>("Empresa", {
+    id: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: DataTypes.INTEGER
+    },
+    cnpj: {
+        type: DataTypes.STRING
+    },
+    nome: {
+        type: DataTypes.STRING
+    },
+    email: {
+        type: DataTypes.STRING,
+        unique: true
+    },
+    senha: {
+        type: DataTypes.STRING
+    },
+    cadastro: {
+        type: DataTypes.DATE
+    },
+    sobre: {
+        type: DataTypes.STRING
+    }
+},
+{
+    tableName: "empresas",
+    freezeTableName: false, 
+    timestamps: false
+});
